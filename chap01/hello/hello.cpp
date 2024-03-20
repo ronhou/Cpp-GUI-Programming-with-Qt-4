0a6dd2b098b3a7c8fb7b219332ae6af7b940f89b
Ronhou <ronhou@qq.com>
2024-03-20 15:32:25 +0800

    QLabel支持带HTML样式的字符串

--- chap01/hello/hello.cpp
+++ chap01/hello/hello.cpp
@@ -5,7 +5,7 @@ int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
     // IDE直接帮我们解决了书中说的内存泄露的问题：使用局部变量即可
-    QLabel label("Hello Qt!");
+    QLabel label("<h2><i>Hello</i> <font color=red>Qt!</font></h2>");
     label.show();
     return app.exec();
 }
